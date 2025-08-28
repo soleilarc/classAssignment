f47ce378cec6024c5cb8867a7e7c72279eac6f43
soleilarc <templeofcheesuscrust@pm.me>
2025-08-28 10:26:33 -0500

    ya like jazz?

--- classAssignment/main.cpp
+++ classAssignment/main.cpp
@@ -3,6 +3,7 @@
 int main() {
 	std::out << "Hello, world!\n";
 	std::out << "D'ya want some Sweet Baby Ray's barbecue sauce?\n"
-		std::out << "No? Okay. :(";
+	std::out << "No? Okay. :(";
+
 	return 0;
 } // End main
