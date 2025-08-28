8d9eb7f18b7d63025021e8a89c5ccf5da3494329
soleilarc <templeofcheesuscrust@pm.me>
2025-08-28 09:44:00 -0500

    Figured it out :D

--- classAssignment/main.cpp
+++ classAssignment/main.cpp
@@ -3,5 +3,6 @@
 int main() {
 	std::cout << "Hello, world!\n";
 	std::cout << "How are you doing today?\n";
+	std::cout << "Okay, I figured out how to use Visual Studio lmao.";
 		return 0;
 } // End main
