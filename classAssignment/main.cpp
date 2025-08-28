e0c46ba716f5fcbf435f86ac2ba666bf00414a67
soleilarc <templeofcheesuscrust@pm.me>
2025-08-28 09:40:22 -0500

    ok hello hi what the heck is going on here

--- classAssignment/main.cpp
+++ classAssignment/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello world!" << std::endl;
+	std::cout << "Hello, world!\n";
+	std::cout << "How are you doing today?\n";
 		return 0;
 } // End main
