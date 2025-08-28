90069ad966b2c44db7bd8a48f65b9ef466ad410e
soleilarc <templeofcheesuscrust@pm.me>
2025-08-28 10:12:01 -0500

    ya like jazz?

--- classAssignment/main.cpp
+++ classAssignment/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, world!\n";
-	std::cout << "How are you doing today?\n";
-	std::cout << "Okay, I figured out how to use Visual Studio lmao.";
-
-	std::cout << "Figured out how to add plugins. Wheeeee! Btw, this is the error line ;)"
+	std::out << "Hello, world!\n";
+	std::out << "D'ya want some Sweet Baby Ray's barbecue sauce?"
 	return 0;
 } // End main
