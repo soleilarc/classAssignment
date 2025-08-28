f75818727f32b1bd15f1b48cf4f5854fc6672cb7
soleilarc <templeofcheesuscrust@pm.me>
2025-08-28 09:47:59 -0500

    ;)

--- classAssignment/main.cpp
+++ classAssignment/main.cpp
@@ -4,5 +4,7 @@ int main() {
 	std::cout << "Hello, world!\n";
 	std::cout << "How are you doing today?\n";
 	std::cout << "Okay, I figured out how to use Visual Studio lmao.";
-		return 0;
+
+	std::cout << "Figured out how to add plugins. Wheeeee! Btw, this is the error line ;)"
+	return 0;
 } // End main
